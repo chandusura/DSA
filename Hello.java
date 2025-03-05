245718e60b3d3dc383b0a85c87631891f7033f6a
chandusura <chandusura7@gmail.com>
2025-03-05 15:22:53 +0530

    third commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ package DSA;
 
 class Hello{
     public  static void main(String[] args) {
-        System.out.println("Hello World!! ");
+        System.out.println("Hello git   World!! ");
+    }
+    public int add(int a,int b){
+        return a+b;
     }
 }
