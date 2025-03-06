d5039b959be4e009e05f02aebcb6d47df42d9e08
chandusura <chandusura7@gmail.com>
2025-03-06 21:17:07 +0530

    06-03-2025

--- DynamicProgramming/LongestCommonSubsequence.java
+++ DynamicProgramming/LongestCommonSubsequence.java
@@ -54,4 +54,22 @@ class Solution2 {
         }
         return dp[index1][index2]=Math.max(rec(index1-1,index2,s1,s2,dp),rec(index1,index2-1,s1,s2,dp));
     }
+
+
+//   Tabulation
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length(),n=text2.length(),i,j;
+        int matrix[][]=new int[m+1][n+1];
+        for(i=1;i<=m;i++){
+            for(j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    matrix[i][j]=matrix[i-1][j-1]+1;
+                }
+                else{
+                    matrix[i][j]=Math.max(matrix[i][j-1],matrix[i-1][j]);
+                }
+            }
+        }
+        return matrix[m][n];
+    }
 }
