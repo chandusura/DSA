85d05a2f0c8ef7fd19f3fcb2a2531051502bf541
chandusura <chandusura7@gmail.com>
2025-03-07 17:52:16 +0530

    07-03-2025

---
+++ DynamicProgramming/longestPalinSubseq.java
@@ -0,0 +1,25 @@
+package DSA.DynamicProgramming;
+
+ 
+ class Solution {
+    public int longestPalinSubseq(String s) {
+        // Reverse the string to compare
+        StringBuilder str = new StringBuilder(s);
+        String s2 = str.reverse().toString();
+        int n = s.length();
+        int dp[][] = new int[n + 1][n + 1];
+
+        // Fill DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) { // Fix inner loop condition
+                if (s.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Fix typo
+                }
+            }
+        }
+
+        return dp[n][n]; // The last cell contains the result
+    }
+}
